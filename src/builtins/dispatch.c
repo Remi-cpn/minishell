2eff0e15e6e22074521706b77831be8542d27e7a
Remi Compain <rcompain@3E5.42angouleme.fr>
2026-01-25 16:51:29 +0100

    Creation dispatch

--- src/builtins/dispatch.c
+++ src/builtins/dispatch.c
@@ -6,14 +6,14 @@
 /*   By: rcompain <rcompain@42angouleme.fr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/25 16:01:39 by rcompain          #+#    #+#             */
-/*   Updated: 2026/01/25 16:14:09 by rcompain         ###   ########.fr       */
+/*   Updated: 2026/01/25 16:29:08 by rcompain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/mini_shell.h"
 
 void	dispatch_builtins(t_ast *ast)
-{	
+{
 	t_ast_normal	*cmd;
 
 	if (ast->kind != NORMAL)
--- src/builtins/echo.c
+++ src/builtins/echo.c
@@ -6,19 +6,33 @@
 /*   By: rcompain <rcompain@42angouleme.fr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/25 14:09:48 by rcompain          #+#    #+#             */
-/*   Updated: 2026/01/25 16:14:51 by rcompain         ###   ########.fr       */
+/*   Updated: 2026/01/25 16:50:53 by rcompain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/mini_shell.h"
 
+static	check_options(char *arg, char )
+{
+
+}
+
 void	echo(char **args)
 {
-	
-	if (!option)
+	const char	*options = "enE";
+	int			i;
+
+	i = 0;
+	while (args[i])
+	{
+		
+	}
+	if (!args[1])
 	{
 		ft_printf("%s\n", txt);
 		return ;
 	}
 
 }
+
+
