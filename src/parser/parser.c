25906fb38533af0a95521b3fa4627623248fdbd0
Von <118017851+Tisaan@users.noreply.github.com>
2026-02-08 03:41:22 +0100

    chose

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -6,7 +6,7 @@
 /*   By: tseche <tseche@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/26 15:17:31 by tseche            #+#    #+#             */
-/*   Updated: 2026/02/07 17:50:47 by tseche           ###   ########.fr       */
+/*   Updated: 2026/02/08 03:35:38 by von              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,6 +50,7 @@ t_ast	**parse(char *src, t_data *shell)
 	t_ast		*tmp;
 	t_src_info	*txt;
 	t_lookup	lookup[13];
+	bool		need_cmd;
 
 	gen_lookup(lookup);
 	txt = ft_calloc(sizeof(t_src_info), 1);
@@ -57,8 +58,15 @@ t_ast	**parse(char *src, t_data *shell)
 		return (NULL);
 	txt->src = src;
 	txt->len = ft_strlen(src);
+	need_cmd = 1;
 	node = ft_calloc(sizeof(t_list *), 1);
 	tmp = parse_expr(lookup, txt);
+	if (need_cmd && ((tmp->kind == AND || tmp->kind == OR)
+	{
+		ft_printf("Syntax error near unexpected token `%c\'\n", txt->src[txt->i]);
+		errno = 1;
+		ft_freenull(tmp.value);
+	}
 	if (tmp && node)
 	{
 		*node = tmp;
@@ -68,7 +76,17 @@ t_ast	**parse(char *src, t_data *shell)
 		{
 			while (ft_iswhitespace(txt->src[txt->i]))
 				txt->i++;
-			tmp = parse_expr(lookup, txt);
+			tmp = parse_expr(lookup, txt)
+			if ((tmp->kind == OR || tnp->kind == AND) && need_cmd)
+					need_cmd = 0;
+			else if (tmp->kind == OR || tmp->kind == AND)
+			{
+				ft_printf("Syntax error near unexpected token `%c\'\n", txt->src[txt->i]);
+				errno = 1;
+				ft_freenull(tmp.value);
+			}
+			else (tmp->kind == CMD)
+					need_cmd = 0;;
 			if (!tmp)
 				break ;
 			ft_lstadd_back((t_list **)node, (t_list *)tmp);
