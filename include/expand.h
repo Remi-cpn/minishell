3054a3daf42f0672ecf7073cd5b2df3305c0ae9e
compain <cpnpro@outlook.fr>
2026-03-10 10:48:57 +0100

    Fix echo bonjour > a b

--- include/expand.h
+++ include/expand.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   expand.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: von <von@student.42.fr>                    +#+  +:+       +#+        */
+/*   By: rcompain <rcompain@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/18 14:05:55 by von               #+#    #+#             */
-/*   Updated: 2026/03/08 15:40:46 by von              ###   ########.fr       */
+/*   Updated: 2026/03/10 10:00:31 by rcompain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,6 +43,7 @@ char	*expand_all_heredoc(char *string, t_data *shell);
 
 //--------------------[WILDCARD]----------------
 int		wildcard(char **, int k, char **tmp, int nbr_files);
+char	**replace_args(char **args, char **add_args, int idx, int *flag);
 int		find_arg_wc(char **args, char ***key);
 void	add_tab_element(char **new_arg, char *cmd_arg, int *flag);
 int		ft_tablen(char **tab);
